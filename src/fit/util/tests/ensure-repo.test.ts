e4b182e6d2f808c594475a63e492810ece6709ea
Emilien Bevierre <emilien.bevierre@couchbase.com>
2026-07-09 17:18:41 +0100

    Use SSH URL for local performer clone

--- src/fit/util/tests/ensure-repo.test.ts
+++ src/fit/util/tests/ensure-repo.test.ts
@@ -6,6 +6,6 @@ import { cloneRepoChoiceLabel } from "../ensure-repo.js";
 test("cloneRepoChoiceLabel includes the repo URL and the target checkout dir", () => {
   assert.equal(
     cloneRepoChoiceLabel(FIT_PERFORMER, "/workspace/transactions-fit-performer"),
-    "Clone it from https://github.com/couchbaselabs/transactions-fit-performer/ to /workspace/transactions-fit-performer",
+    "Clone it from git@github.com:couchbaselabs/transactions-fit-performer.git to /workspace/transactions-fit-performer",
   );
 });
